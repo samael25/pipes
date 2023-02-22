a1edf6bf4d80507a08052feaafa59dd3c71601fe
samael <anzua_luis@hotmail.com>
2023-02-22 10:32:32 -0600

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,151 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  hey: string = 'luis anzua';
+
+  loco() {
+    console.log(this.hey);
+    }
+    countries!: any[];
+
+selectedCity1: any;
+
+selectedCity2: any;
+
+value!: Date;
+date1!: Date;
+
+date2!: Date;
+
+date3!: Date;
+
+date4!: Date;
+
+date5!: Date;
+
+date6!: Date;
+
+date7!: Date;
+
+date8!: Date;
+
+date9!: Date;
+
+date10!: Date;
+
+date11!: Date;
+
+date12!: Date;
+
+date13!: Date;
+
+date14!: Date;
+
+dates!: Date[];
+
+rangeDates!: Date[];
+
+minDate!: Date;
+
+maxDate!: Date;
+
+invalidDates!: Array<Date>
+
+ngOnInit() {
+    this.countries = [
+        {
+            name: 'Australia',
+            code: 'AU',
+            states: [
+                {
+                    name: 'New South Wales',
+                    cities: [
+                        {cname: 'Sydney', code: 'A-SY'},
+                        {cname: 'Newcastle', code: 'A-NE'},
+                        {cname: 'Wollongong', code: 'A-WO'}
+                    ]
+                },
+                {
+                    name: 'Queensland',
+                    cities: [
+                        {cname: 'Brisbane', code: 'A-BR'},
+                        {cname: 'Townsville', code: 'A-TO'}
+                    ]
+                },
+
+            ]
+        },
+        {
+            name: 'Canada',
+            code: 'CA',
+            states: [
+                {
+                    name: 'Quebec',
+                    cities: [
+                        {cname: 'Montreal', code: 'C-MO'},
+                        {cname: 'Quebec City', code: 'C-QU'}
+                    ]
+                },
+                {
+                    name: 'Ontario',
+                    cities: [
+                        {cname: 'Ottawa', code: 'C-OT'},
+                        {cname: 'Toronto', code: 'C-TO'}
+                    ]
+                },
+
+            ]
+        },
+        {
+            name: 'United States',
+            code: 'US',
+            states: [
+                {
+                    name: 'California',
+                    cities: [
+                        {cname: 'Los Angeles', code: 'US-LA'},
+                        {cname: 'San Diego', code: 'US-SD'},
+                        {cname: 'San Francisco', code: 'US-SF'}
+                    ]
+                },
+                {
+                    name: 'Florida',
+                    cities: [
+                        {cname: 'Jacksonville', code: 'US-JA'},
+                        {cname: 'Miami', code: 'US-MI'},
+                        {cname: 'Tampa', code: 'US-TA'},
+                        {cname: 'Orlando', code: 'US-OR'}
+                    ]
+                },
+                {
+                    name: 'Texas',
+                    cities: [
+                        {cname: 'Austin', code: 'US-AU'},
+                        {cname: 'Dallas', code: 'US-DA'},
+                        {cname: 'Houston', code: 'US-HO'}
+                    ]
+                }
+            ]
+        }
+    ];
+
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let prevMonth = (month === 0) ? 11 : month -1;
+    let prevYear = (prevMonth === 11) ? year - 1 : year;
+    let nextMonth = (month === 11) ? 0 : month + 1;
+    let nextYear = (nextMonth === 0) ? year + 1 : year;
+    this.minDate = new Date();
+    this.minDate.setMonth(prevMonth);
+    this.minDate.setFullYear(prevYear);
+    this.maxDate = new Date();
+    this.maxDate.setMonth(nextMonth);
+    this.maxDate.setFullYear(nextYear);
+
+    let invalidDate = new Date();
+    invalidDate.setDate(today.getDate() - 1);
+    this.invalidDates = [today,invalidDate];
 }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CalendarModule} from 'primeng/calendar';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import {ButtonModule} from 'primeng/button';
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import {CascadeSelectModule} from 'primeng/cascadeselect';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimeNgModule,
+    CascadeSelectModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    ToggleButtonModule,
+    ButtonModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
